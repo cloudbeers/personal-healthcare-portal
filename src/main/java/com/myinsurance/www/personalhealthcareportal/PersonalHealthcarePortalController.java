8054bd59ee851afde83e0b624047ce22b81fdb2b
Sylvain Deyris <sdeyris@cloudbees.com>
2018-12-11 19:19:33 +0100

    PHP-23 added to index

--- src/main/java/com/myinsurance/www/personalhealthcareportal/PersonalHealthcarePortalController.java
+++ src/main/java/com/myinsurance/www/personalhealthcareportal/PersonalHealthcarePortalController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonalHealthcarePortalController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Personal Healthcare Portal PHP-22";
+        return "Greetings from Personal Healthcare Portal PHP-22 and PHP-23";
     }
 }
