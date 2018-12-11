ad339ccd66268e19b0b2897590100fc6fd7c63d7
Sylvain Deyris <sdeyris@cloudbees.com>
2018-12-11 19:34:44 +0100

    Removed PHP-22 and PHP-23 from index to trigger a new release note generation

--- src/main/java/com/myinsurance/www/personalhealthcareportal/PersonalHealthcarePortalController.java
+++ src/main/java/com/myinsurance/www/personalhealthcareportal/PersonalHealthcarePortalController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonalHealthcarePortalController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Personal Healthcare Portal PHP-22 and PHP-23";
+        return "Greetings from Personal Healthcare Portal";
     }
 }
